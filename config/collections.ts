eeb9b8109eeec2adfb33f764d16839ddb79660be
Bruno Viana <brunovlviana41@gmail.com>
2026-01-14 00:05:49 -0300

    atualização de site suplementos

--- config/collections.ts
+++ config/collections.ts
@@ -1,6 +1,6 @@
 export const collections = [
-  { id: 1, name: "Linha Landerlan", image: "/images/landerlan.jpg" },
-  { id: 2, name: "Linha Cooper", image: "/images/cooper.jpg" },
-  { id: 3, name: "Linha Monstro", image: "/images/hero.jpg" },
+  { id: 1, name: "Linha Growth", image: "/images/growth.png" },
+  { id: 2, name: "Linha Optimum Nutrition", image: "/images/on.png" },
+  { id: 3, name: "Linha Integral Medica", image: "/images/integral.png" },
   { id: 4, name: "Linha Premium", image: "/images/hero.jpg" },
 ];
