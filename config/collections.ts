ace3b8f474c618456d3efeeb63e7f1ea67040576
Bruno Viana <brunovlviana41@gmail.com>
2026-02-18 15:57:15 -0300

    Atualiza dependências e collections

--- config/collections.ts
+++ config/collections.ts
@@ -1,5 +1,5 @@
 export const collections = [
-  { id: 1, name: "Linha Growth", image: "/images/rgouwth.png" },
-  { id: 2, name: "Linha Optimum Nutrition", image: "/images/on.png" },
+  { id: 1, name: "Linha Lander Gold", image: "/images/lander premium.png" },
+  { id: 2, name: "Linha Landerla", image: "/images/landerlan.jpg" },
   { id: 3, name: "Linha Integral Medica", image: "/images/integral.png" },
 ];
